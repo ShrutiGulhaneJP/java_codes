aeb4e228491ad0add85bc1449ccbd87b7f1688ab
ShrutiGulhaneJP <130272800+ShrutiGulhaneJP@users.noreply.github.com>
2023-07-03 18:48:41 +0530

    Add files via upload

---
+++ six.java
@@ -0,0 +1,16 @@
+public class Six
+   { 
+    public static void main (String args[ ]) 
+    { 
+        
+        int marks=45;
+		if(marks>=40)
+		{
+		System.out.println("Pass");
+		}
+		else
+		{
+		System.out.println("Fail");
+		}
+	}
+   }
