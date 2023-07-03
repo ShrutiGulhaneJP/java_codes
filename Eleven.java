ded8e6385c82beea4ac13e382f59fc2ec0c1c348
ShrutiGulhaneJP <130272800+ShrutiGulhaneJP@users.noreply.github.com>
2023-07-03 18:51:51 +0530

    Add files via upload

---
+++ Eleven.java
@@ -0,0 +1,14 @@
+class Eleven
+{
+public static void main (String args[])
+{
+int num1=20;
+int num2=40;
+int max;
+max=(num1>num2)? num1 : num2;
+System.out.println("largest number is:"+max);
+
+}
+
+
+}
