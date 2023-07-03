fe569c744d508875713a1bc8b0e4962ee97325ce
ShrutiGulhaneJP <130272800+ShrutiGulhaneJP@users.noreply.github.com>
2023-07-03 18:53:01 +0530

    Add files via upload

---
+++ Thirteen.java
@@ -0,0 +1,15 @@
+class Thirteen
+{
+ public static void main(String args[])
+ {
+  for(int i = 1; i<=5; i++)
+  {
+  if(i==3)
+{
+      continue;
+}  
+  }
+  
+ }
+     system.out.println("Hello");
+}
